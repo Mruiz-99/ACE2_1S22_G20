b10755df85ac02a8daf7a41a08a1b45c4295f32e
Bvhdc <40047058+Bvhdc@users.noreply.github.com>
2022-04-25 17:57:56 -0600

    arduino update

--- Proyecto 2/Proteus Test/Arduino/proyecto2/proyecto2.ino
+++ Proyecto 2/Proteus Test/Arduino/proyecto2/proyecto2.ino
@@ -1,14 +1,16 @@
+#include <Servo.h>
+
 // Temporales para pruebas, cambiarlas o quitarlas de ser necesario
 #include <DHT.h>
 #include <DHT_U.h>
 #define DHTPIN 2
 #define DHTTYPE DHT11
-int TEMPPIN = A0;
+float TEMPPIN = A0;
 // Fin de Temporales
 
 bool Llave = false; // Falso = cerrada
 bool Chispa = false; // Falso = inactiva
-
+Servo servoMotor;//inicializacion motor
 // Temporal 
 DHT dht(DHTPIN,DHTTYPE);
 // Fin Temporal
@@ -16,9 +18,10 @@ DHT dht(DHTPIN,DHTTYPE);
 void setup() {
   // Serial se usara para la comunicacion bt
   Serial.begin(9600);
-
+  pinMode(13,OUTPUT);
   //Temporal
   dht.begin();
+  servoMotor.attach(7);
   // Fin Temporal
 
   
@@ -31,7 +34,7 @@ void loop() {
   // Temperatura y Metano
     /* Codigo temporal para pruebas */
     float temp = dht.readTemperature();
-    float met = analogRead(TEMPPIN)*5*100/1024;
+    float met = analogRead(TEMPPIN);
   
   /* Envia valores por bluetooth */
   
@@ -47,6 +50,7 @@ void loop() {
   delay(100);
   
   Serial.print("METANO ");
+  met=getMethane();
   Serial.println(met);
 
   /* Leer comandos recibidos por bluetooth */
@@ -62,17 +66,42 @@ void loop() {
     if(command == 'L'){
       Llave = true;
       // Codigo que abre la llave de paso
+      openGas();
     }else if(command == 'M'){
       Llave = false;
       // Codigo que cierra la llave de paso
+      closeGas();
     }else if(command == 'C'){
       Chispa = true;
       // Codigo que enciende la chispa
+      digitalWrite(13, HIGH);
     }else if(command == 'D'){
       Chispa = false;
       // Codigo que apaga la chispa
+      digitalWrite(13, LOW);
     }
   }
 
   delay(800);
 }
+float getMethane(){
+  int R0=945;
+  float metRead=analogRead(TEMPPIN);
+  float v=metRead * 5/1023;
+  float Rs=(5-v)*1000/v;
+  float ppm=pow(Rs/R0,-2.95)*1000;
+  return ppm;
+  }
+float openGas(){
+   // Desplazamos a la posición 0º
+  servoMotor.write(0);
+  // Esperamos 1 segundo
+  delay(1000);
+  
+  // Desplazamos a la posición 90º
+  servoMotor.write(90);
+  }
+float closeGas(){
+   // Desplazamos a la posición 0º
+  servoMotor.write(0);
+  }
