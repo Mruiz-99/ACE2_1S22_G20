11e425d33c04a57e8b8574937fa54244a2fbe844
Stevengez <steven.jsg@gmail.com>
2022-04-29 19:31:11 -0600

    Fixed Instant crash in android 8

--- Proyecto 2/EstufaDroid/app/src/main/java/com/s7evensoftware/estufadroid/MainActivity.kt
+++ Proyecto 2/EstufaDroid/app/src/main/java/com/s7evensoftware/estufadroid/MainActivity.kt
@@ -25,6 +25,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //binding = ActivityMainBinding.inflate(layoutInflater)
         binding = ActivityMainBinding.inflate(layoutInflater)
         prepareViews()
         setContentView(binding.root)
